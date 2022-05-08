260c75f47f9cdba28fe30c63de40415befc86eed
Suman Achary <suman.achary95@gmail.com>
2022-05-09 00:04:38 +0530

    is node present

---
+++ algorithms/BTrees/isNodePresent.py
@@ -0,0 +1,16 @@
+def isNodePresent(root, x):
+    if root == None:
+        return
+    if root.data == x:
+        return True
+    res = isNodePresent(root.left, x)
+    if res:
+        return True
+    res2 = isNodePresent(root.right, x)
+    return res2
+
+
+
+
+
+
--- algorithms/BTrees/printNodes_at_depth_K.py
+++ algorithms/BTrees/printNodes_at_depth_K.py
@@ -5,3 +5,14 @@ def printDepthK(root,k):
         return root.data 
     printDepthK(root.left,k-1)
     printDepthK(root.right,k-1)
+
+
+def printDepthK_2(root,k,d=0):
+    if root == None:
+        return 
+    if k == d:
+        print(root.data)
+        return
+    printDepthK_2(root.left,k,d+1)
+    printDepthK_2(root.right,k,d+1)
+
