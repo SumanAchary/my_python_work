ac8083b3dafdb2571711c7471265e920dd3c2b28
Suman Achary <suman.achary95@gmail.com>
2022-05-15 21:52:22 +0530

    remove Leaves for binary tree

---
+++ algorithms/BTrees/removeLeaf.py
@@ -0,0 +1,8 @@
+def removeLeaves(root):
+    if root == None:
+        return None
+    if root.left == None and root.right == None:
+        return None
+    root.left = removeLeaves(root.left)
+    root.right = removeLeaves(root.right)
+    return root
